53af25a1b0d4deedee310add33274cd0ac2db684
Nicolás Quiroz <nh.quiroz@gmail.com>
2017-06-15 18:22:20 -0300

    Add 'Simple Array Sum' solution

---
+++ Algorithms/Warmup/simpleArraySum.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n, currentNumber, sum int
+
+	fmt.Scan(&n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&currentNumber)
+		sum += currentNumber
+	}
+	fmt.Print(sum)
+}
