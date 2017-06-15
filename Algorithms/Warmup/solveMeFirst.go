6fc9de98f11ab50707f23e3a7e1ac571a15f891e
Nicolás Quiroz <nh.quiroz@gmail.com>
2017-06-15 15:37:40 -0300

    Add 'Solve Me First' solution

--- Algorithms/Warmup/solveMeFirst.go
+++ Algorithms/Warmup/solveMeFirst.go
@@ -1,1 +1,16 @@
+package main
 
+import "fmt"
+
+func solveMeFirst(a, b uint32) uint32 {
+	return a + b
+}
+
+func main() {
+	var a, b, res uint32
+
+	fmt.Scanf("%v\n%v", &a, &b)
+
+	res = solveMeFirst(a, b)
+	fmt.Println(res)
+}
