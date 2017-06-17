9a4acf00c292b876945c36b7d5e68cd71c50e40b
Nicolás Quiroz <nh.quiroz@gmail.com>
2017-06-17 15:30:28 -0300

    Add 'Compare the Triplets' solution

---
+++ Algorithms/Warmup/compareTheTriplets.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a0, a1, a2 int
+	var b0, b1, b2 int
+	fmt.Scanf("%d %d %d\n", &a0, &a1, &a2)
+	fmt.Scanf("%d %d %d\n", &b0, &b1, &b2)
+
+	ar0, br0 := comparePoints(a0, b0)
+	ar1, br1 := comparePoints(a1, b1)
+	ar2, br2 := comparePoints(a2, b2)
+
+	alicePoints := ar0 + ar1 + ar2
+	bobPoints := br0 + br1 + br2
+
+	fmt.Printf("%d %d", alicePoints, bobPoints)
+}
+
+func comparePoints(ai, bi int) (aRes, bRes int) {
+	if ai > bi {
+		return 1, 0
+	} else if bi > ai {
+		return 0, 1
+	} else {
+		return 0, 0
+	}
+}
